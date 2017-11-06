4b70cbd74ea9fd92990557103a3db3fe141cf1e1
Neosoft <neosoft@Neosofts-Mac-mini.local>
2017-11-06 18:17:51 +0530

    added selection reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import LibraryReducer from './LibraryReducer';
+import selectionReducer from './selectionReducer';
 
 export default combineReducers({
-    libraries: LibraryReducer
+    libraries: LibraryReducer,
+    selectedLibraryId: selectionReducer
 })
