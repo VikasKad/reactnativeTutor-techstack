f8915222131f9cca9f97ea1c72b893a799cafd4e
Neosoft <neosoft@Neosofts-Mac-mini.local>
2017-11-06 16:33:11 +0530

    worked on add Redux functionality and connector use

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './src/reducers';
 import {Header} from './src/components/common';
-
+import LibraryList from './src/components/LibraryList';
 export default class App extends React.Component {
     render() {
         return (
             <Provider store={createStore(reducers)}>
                 <View>
                     <Header headerText={'Tech Stack'}></Header>
+                    <LibraryList></LibraryList>
                 </View>
             </Provider>
 
